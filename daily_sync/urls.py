d00cea10d28ed0153fc337651f9ac2d6a53a10d1
KristianColville1 <kristiancolville1@gmail.com>
2022-07-27 20:36:17 +0100

    Update url patterens for project

--- daily_sync/urls.py
+++ daily_sync/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('feed.urls'), name='feed'),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls'), name='home'),
+    path('feed/', include('feed.urls'), name='feed'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
