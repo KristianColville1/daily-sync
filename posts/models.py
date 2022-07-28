d393bd803b82548fe38395e5e3a01c5e424ddcfe
KristianColville1 <kristiancolville1@gmail.com>
2022-07-28 07:02:31 +0100

    Created a model for a post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Posted"))
+class Post(models.Model):
+  """
+  Post model
+  """
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
+  title = models.TextField(max_length=200, unique=True)
+  content = models.TextField(max_length=500)
+  created_on = models.DateTimeField(auto_now_add=True)
+  updated_on = models.DateTimeField(auto_now_add=True)
+  edited = models.BooleanField(default=False)
+  likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+  status = models.IntegerField(choices=STATUS, default=0)
+  slug= models.SlugField()
+  
+  class Meta:
+    ordering = ['-created_on']
+
+  def __str__(self):
+    return self.content
