48664f2f8e38f6f420ee01c25b47505520f4bd68
KristianColville1 <kristiancolville1@gmail.com>
2022-07-29 07:42:32 +0100

    Added get and post methods for feed view

--- feed/views.py
+++ feed/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 from django.views import View, generic
 from posts.models import Post
+from posts.forms import PostForm
+
+
 class FeedView(View):
     """
     FeedView class
     """
+
     def get(self, request, *args, **kwargs):
-        test = Post.objects.filter(status=1)
-        test2 = Post.objects.filter(status=1)
-        events = []
-        events.extend(test)
-        events.extend(test2)
-        events = list(dict.fromkeys(events))
-        return render(request, 'feed/index.html', {'events': events})
+        post = Post.objects.filter(status=1)
+        form = PostForm()
+        context = {
+            'posts': post,
+            'form': form
+        }
+        return render(request, 'feed/index.html', context)
     
+    def post(self, request, *args, **kwargs):
+        post = Post.objects.filter(status=1)
+        form = PostForm()
+        context = {
+            'posts': post,
+            'form': form
+        }
+        
+        post_form = PostForm(data=request.POST)
+        
+        if post_form.is_valid():
+            post_form.instance.author = request.user
+            post_form.instance.status = 1
+            post_form.save()
+        return render(request, 'feed/index.html', context)
