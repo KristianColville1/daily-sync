90325bbf4d7f7a39723d5d2db54f130ef7e0d7d4
KristianColville1 <kristiancolville1@gmail.com>
2022-07-30 10:28:28 +0100

    Created a comment class for user posts

--- posts/models.py
+++ posts/models.py
@@ -6,24 +6,52 @@ STATUS = ((0, "Draft"), (1, "Posted"))
 
 class Post(models.Model):
     """
-  Post model
-  """
+    Post model
+    """
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
-                               related_name="user_posts", null=True)
+                               related_name="user_posts",
+                               null=True)
     title = models.TextField(max_length=200, unique=True)
     content = models.TextField(max_length=500)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
-    likes = models.ManyToManyField(User,
-                                   related_name='liked_posts',
-                                   blank=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    slug = models.SlugField(max_length=200)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    status = models.IntegerField(choices=STATUS, default=1)
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.title
+
+    def count_likes(self):
+        return self.likes
+
+
+class Comment(models.Model):
+    """
+    Comment model class
+    """
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name="user_comments",
+                               null=True)
+    comment = models.TextField(max_length=300)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
+    likes = models.ManyToManyField(User,
+                                   related_name='comment_likes',
+                                   blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.comment
+
+    def count_likes(self):
+        return self.likes
