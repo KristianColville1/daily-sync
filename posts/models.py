d2cadf2d665d0bcdbc70c896d788e964fc29c6a3
KristianColville1 <kristiancolville1@gmail.com>
2022-07-31 16:01:16 +0100

    Added autogenerate slugfield to create slugs

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,5 @@
+from enum import unique
+from autoslug import AutoSlugField
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,12 +10,12 @@ class Post(models.Model):
     """
     Post model
     """
-    slug = models.SlugField(max_length=200, auto_created=True)
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name="user_posts",
                                null=True)
     title = models.TextField(max_length=200, unique=True)
+    slug = AutoSlugField(populate_from='title')
     content = models.TextField(max_length=500)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
@@ -37,7 +39,9 @@ class Comment(models.Model):
     """
     Comment model class
     """
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name="comments")
     name = models.CharField(max_length=100)
     comment = models.TextField(max_length=300)
     created_on = models.DateTimeField(auto_now_add=True)
