97c4a55ecac25f1f98fd3de85a7e59248e18567f
KristianColville1 <kristiancolville1@gmail.com>
2022-07-31 13:43:32 +0100

    Added a commentform class and widgets for layout

--- posts/forms.py
+++ posts/forms.py
@@ -1,20 +1,46 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
+
 
 class PostForm(forms.ModelForm):
-  """
+    """
   Creates a form object for the post model
   """
-  class Meta:
-    model = Post
-    fields = ('title', 'content',)
-    widgets = {
-      'title': forms.Textarea(attrs={
-        'rows': 1,
-        'placeholder': 'Write the title of your post here'
-      }),
-      'content': forms.Textarea(attrs={
-        'rows': 6,
-        'placeholder': 'Write your post here'
-      })
-    }  
+
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            'content',
+        )
+        widgets = {
+            'title':
+            forms.Textarea(attrs={
+                'rows': 1,
+                'placeholder': 'Write the title of your post here'
+            }),
+            'content':
+            forms.Textarea(attrs={
+                'rows': 6,
+                'placeholder': 'Write your post here'
+            })
+        }
+
+
+class CommentForm(forms.ModelForm):
+    """
+  Creates a form object for the comment model
+  """
+
+    class Meta:
+        model = Comment
+        fields = (
+            'comment',
+        )
+        widgets = {
+            'comment':
+            forms.Textarea(attrs={
+                'rows': 6,
+                'placeholder': 'Write your comment here'
+            })
+        }
