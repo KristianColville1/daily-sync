253d0440cbbf41592bbc71fc0562901b5b154d2a
KristianColville1 <kristiancolville1@gmail.com>
2022-08-02 09:52:19 +0100

    Added sliders for the post options and settings dropdowns

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,17 +1,36 @@
-$(document).ready(()=> {
+$(document).ready(() => {
 
-  /* Switches icons for the menu when button clicked open and close*/
-  $('#menu-toggle').on('click', () => {
-      let toggle_on = 'i.fa-bars';
-      let toggle_off = 'i.fa-x';
+    /* Switches icons for the menu when button clicked open and close*/
+    $('#menu-toggle').on('click', () => {
+        let toggle_on = 'i.fa-bars';
+        let toggle_off = 'i.fa-x';
 
-      if ($(toggle_off).hasClass('d-none')){
-          $(toggle_off).removeClass('d-none');
-          $(toggle_on).addClass('d-none').fadeIn(1300);
+        if ($(toggle_off).hasClass('d-none')) {
+            $(toggle_off).removeClass('d-none');
+            $(toggle_on).addClass('d-none').fadeIn(1300);
 
-      }  else if ($(toggle_on).hasClass('d-none')){
-          $(toggle_on).removeClass('d-none');
-          $(toggle_off).addClass('d-none').fadeIn(1300);
-      } 
-  });
+        } else if ($(toggle_on).hasClass('d-none')) {
+            $(toggle_on).removeClass('d-none');
+            $(toggle_off).addClass('d-none').fadeIn(1300);
+        }
+    });
+
+    /* Slides the dropdown menu for user settings */
+    $('.nav-menu').on('click', () => {
+        if ($('.nav-menu').hasClass('show')) {
+            $('.nav-menu-items').slideUp();
+        } else if (!$('.nav-menu').hasClass('show')) {
+            $('.nav-menu-items').slideDown();
+        }
+    });
+
+    /* Slides the dropdown menu for posts for editing, deleting and reporting */
+    $('.post-options-menu').on('click', () => {
+        if ($('.post-options-menu').hasClass('show')) {
+            $('.post-option-list').slideUp();
+        } else if (!$('.post-options-menu').hasClass('show')) {
+            $('.post-option-list').slideDown();
+        }
+    });
+
 });
