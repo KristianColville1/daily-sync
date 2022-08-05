0f6f684a5e303d086a5127c762ec3553e97d3950
KristianColville1 <kristiancolville1@gmail.com>
2022-08-05 18:09:03 +0000

    Update email settings in settings.py

--- daily_sync/settings.py
+++ daily_sync/settings.py
@@ -66,9 +66,9 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_AUTHENTICATION = "username_email"
 ACCOUNT_USERNAME_MIN_LENGTH = 5
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ.get("MAILGUN_SMTP_SERVER")
-EMAIL_PORT = os.environ.get("MAILGUN_SMTP_PORT")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("MAILGUN_SMTP_LOGIN")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_SMTP_PASSWORD")
 EMAIL_USE_TLS = True
