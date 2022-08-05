a58d7e0793f82355027c8eaf0454075c2285403b
KristianColville1 <kristiancolville1@gmail.com>
2022-08-05 15:01:28 +0000

    Created class to check for symbols in passwords

--- password_validation/validator.py
+++ password_validation/validator.py
@@ -17,3 +17,20 @@ class NumValidator(object):
     def get_help_text(self):
         return text("Your password must contain at least 1 number, 0-9.")
 
+
+class SymbolValidator(object):
+    """
+    SymbolValidator checks passwords for symbols
+    """
+
+    def validate(self, password, user=None):
+        if not re.findall('["#$%&\'()*+,-./:;<=>?@\\^_`{|}~]', password):
+            raise ValidationError(text(
+                "The password must contain at least 1 special character: " +
+                "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"),
+                                  code='password_no_symbol')
+
+    def get_help_text(self):
+        return text(
+            "Your password must contain at least 1 special character: " +
+            "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?")
