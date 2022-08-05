da4a4d568ccb24ae81f14731f62f8199e6f3be68
KristianColville1 <kristiancolville1@gmail.com>
2022-08-05 09:58:46 +0000

    Validate comment forms for the feedViewPost path

--- feed/views.py
+++ feed/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render
 from django.views import View
 from django.contrib import messages
 from posts.models import Post
-from posts.forms import PostForm
+from posts.forms import PostForm, CommentForm
 
 
 class FeedViewIndex(View):
@@ -42,20 +42,35 @@ class FeedViewPost(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        form = PostForm()
-        context = {'post': post, 'form': form}
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        context = {
+            'post': post,
+            'comments': comments,
+            'comment_form': CommentForm()
+        }
         return render(request, 'posts/view_post.html', context)
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        form = PostForm()
-        context = {'post': post, 'form': form}
-
-        post_form = PostForm(data=request.POST)
-
-        if post_form.is_valid():
-            post_form.instance.author = request.user
-            post_form.instance.status = 1
-            post_form.save()
-        return render(request, 'posts/index.html', context)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        context = {
+            'post': post,
+            'comments': comments,
+            'comment_form': CommentForm()
+        }
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.name = request.user.username
+            comment_form.instance.email = request.user.email
+            comment_form.instance.approved = True
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Your comment has been submitted')
+        else:
+            messages.add_message(
+                request, messages.ERROR,
+                'Oops something has went wrong, please try again!')
+        return render(request, 'posts/view_post.html', context)
