fdd7279322b5492d29eb9058e45a006716dd844a
KristianColville1 <kristiancolville1@gmail.com>
2022-08-05 15:06:57 +0000

    Created class to check for uppercase letters in passwords

--- password_validation/validator.py
+++ password_validation/validator.py
@@ -34,3 +34,22 @@ class SymbolValidator(object):
         return text(
             "Your password must contain at least 1 special character: " +
             "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?")
+
+
+class UppercaseValidator(object):
+    """
+    UppercaseValidator checks passwords for uppercase letters
+    """
+
+    def validate(self, password, user=None):
+        if not re.findall('[A-Z]', password):
+            raise ValidationError(
+                text(
+                    "The password must contain at least 1 capital letter, A-Z."
+                ),
+                code='password_no_upper',
+            )
+
+    def get_help_text(self):
+        return text(
+            "Your password must contain at least 1 capital letter, A-Z.")
