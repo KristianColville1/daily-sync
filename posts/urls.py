9eff0a7e6fbb0dec2e3a505539adc6cab581c203
KristianColville1 <kristiancolville1@gmail.com>
2022-08-07 10:44:20 +0000

    Created form for editing a post and the url

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.get_posts, name='posts'),
-    path('delete/<int:post_id>/',
-         views.delete_post,
-         name='delete_post')
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
 ]
