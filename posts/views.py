3fdf9b4a26228c47f87c0c806c594b202b9b7acb
KristianColville1 <kristiancolville1@gmail.com>
2022-08-07 10:43:42 +0000

    Added the ability to update a post

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib import messages
 from .models import Post
+from .forms import PostForm
 
 
 def get_posts(request):
@@ -15,3 +16,15 @@ def delete_post(request, post_id):
     messages.add_message(request, messages.SUCCESS,
                          'Your post has been deleted')
     return redirect('/feed/')
+
+
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+        return redirect('/feed/')
+    form = PostForm(instance=post)
+    context = {'form': form}
+    return render(request, 'posts/edit_post.html', context)
