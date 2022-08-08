150dd6d5a3ef2111686487415285603e049a9a1f
KristianColville1 <kristiancolville1@gmail.com>
2022-08-08 13:39:24 +0000

    Reset database and update profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,6 @@
-from autoslug import AutoSlugField
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from posts.models import Post
 
 
 class Profile(models.Model):
@@ -13,20 +11,23 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=30, blank=True, null=True)
     email = models.CharField(max_length=320, unique=True)
     d_o_b = models.DateField(blank=True, null=True)
-    username = models.OneToOneField(User,
-                                    on_delete=models.CASCADE,
-                                    related_name='profiles')
+    bio = models.TextField(max_length=200, unique=False)
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                related_name='profiles')
     avatar = CloudinaryField('avatar',
                              folder='avatars',
                              null=True,
                              blank=True,
                              default='/static/img/default-profile-image.png')
-    posts = models.ManyToManyField(Post, related_name="posts")
     friends = models.ManyToManyField('self',
                                      blank=True,
+                                     symmetrical=True,
                                      related_name='friends')
-    bio = models.TextField(max_length=800, unique=False)
-    slug = AutoSlugField(populate_from=username, unique=True)
+    follows = models.ManyToManyField('self',
+                                     blank=True,
+                                     symmetrical=False,
+                                     related_name='follows')
 
     def __str__(self):
-        return self.username
+        return self.user.username
