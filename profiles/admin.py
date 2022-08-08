42c954f7b4e99b8a9a1369eefeecdf756508ebea
KristianColville1 <kristiancolville1@gmail.com>
2022-08-08 19:21:13 +0000

    Register posts, comments and profile under user in profiles/admin.py

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,7 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from posts.models import Post, Comment
 from .models import Profile
 
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'email']
+class PostInlineAdmin(admin.StackedInline):
+    """
+    PostInlineAdmin mixin to display posts in user inside admin panel.
+    Provides a cleaner interface to work with. Uses Summernote to provide a
+    better editer.
+    """
+    model = Post
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+
+
+class CommentInlineAdmin(admin.StackedInline):
+    """
+    CommentInlineAdmin mixin to display posts in user inside admin panel.
+    Provides a cleaner interface to work with. Uses Summernote to provide a
+    better editer.
+    """
+    model = Comment
+    list_display = ('content', 'created_on')
+    search_fields = ['content']
+
+
+class ProfileInlineAdmin(admin.StackedInline):
+    """
+    ProfileInlineAdmin class
+    """
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    """
+    UserAdmin provides a better site owner experience to manage the various
+    models that a user can create
+    """
+    model = User
+    fields = ['username', 'email']
+    inlines = [ProfileInlineAdmin, PostInlineAdmin, CommentInlineAdmin]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
