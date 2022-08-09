e3471995e725d2b2fbdad0a951db18c40c2c0475
KristianColville1 <kristiancolville1@gmail.com>
2022-08-09 11:56:37 +0000

    Render profileownerview with profile model in profiles/index.html

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Profile
+from posts.models import Post
 
 
 class ProfileOwnerView(View):
     """
-    ProfileView class renders a users profile
+    ProfileOwnerView class renders a users profile
     """
 
     def get(self, request, *args, **kwargs):
+        post = Post.objects.filter(author=request.user)
         context = {
-            'profile': Profile()
+            'profile': Profile(),
+            'posts': post
         }
         return render(request, 'profiles/index.html', context)
