791bc38288733df5d89e714c8dd79e489d99c1a4
KristianColville1 <kristiancolville1@gmail.com>
2022-08-11 06:52:21 +0000

    Targeted comment form for new styling

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,9 +1,18 @@
-
 /** 
  * comment form
  function collects the post id and manipulates
- the appropriate container with the element to select */  
+ the appropriate container with the element to select */
 var commentForm = (postID) => {
     let selector = `.comment-form-${postID}`
     $(selector).removeClass('d-none');
 }
+
+$(document).ready(() => {
+    /**
+     * Styles comment form
+     */
+    let commentContainer = $('#div_id_comment');
+    let comment = $('#id_comment');
+    $(comment).addClass('dark-boxes rounded-5 text-center');
+    $(commentContainer).children().first().addClass('d-none')
+})
