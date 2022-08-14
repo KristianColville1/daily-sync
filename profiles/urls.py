a3597fe41b46a8150d67bde542b39dd4aad6a0cc
KristianColville1 <kristiancolville1@gmail.com>
2022-08-14 23:18:06 +0000

    Added url path to view other profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.ProfileOwnerView.as_view(), name='profile'),
+    path('view_profile/<slug:slug>/',
+         views.ProfileView.as_view(),
+         name='user_profile'),
     path('user_profiles/', views.ProfilesView.as_view(), name='user_profiles'),
 ]
