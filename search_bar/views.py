a35ea1eebccde145e859558415fea56545e05a1d
KristianColville1 <kristiancolville1@gmail.com>
2022-08-16 12:15:06 +0000

    Added views and urls for the search box results

--- search_bar/views.py
+++ search_bar/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def search(request):
+    """
+    Search fuction view to view search results
+    """
+    if request.method == "POST":
+        searched = request.POST['searched']
+        return render(request, 'search_bar/search.html',
+                      {'searched': searched})
+    else:
+        return render(request, 'search_bar/search.html', {})
