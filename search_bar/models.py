b729746ce4e351a9bf9b3b076a4cd77b9211e833
KristianColville1 <kristiancolville1@gmail.com>
2022-08-16 18:11:35 +0000

    Added search model with array field to store searches

--- search_bar/models.py
+++ search_bar/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class SearchModel(models.Model):
+    """
+    Search model to store user searches
+    """
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name="searches",
+                             null=True)
+    searches = ArrayField(models.models.CharField(max_length=30, blank=True))
