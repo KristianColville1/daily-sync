472d8b4289b217a36824f85b57959de8db131580
KristianColville1 <kristiancolville1@gmail.com>
2022-08-17 11:19:00 +0000

    Remove unused search model

--- search_bar/models.py
+++ search_bar/models.py
@@ -1,14 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.contrib.postgres.fields import ArrayField
 
-
-class SearchModel(models.Model):
-    """
-    Search model to store user searches
-    """
-    user = models.ForeignKey(User,
-                             on_delete=models.CASCADE,
-                             related_name="searches",
-                             null=True)
-    searches = ArrayField(models.models.CharField(max_length=30, blank=True))
+# Create your models here.
