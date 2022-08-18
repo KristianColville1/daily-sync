17bc52b984a14498027ce86363d7afa6485cf118
KristianColville1 <kristiancolville1@gmail.com>
2022-08-18 19:12:35 +0000

    Created chat model for handling chats

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Chat(models):
+    """
+    Chat model
+    """
+    content = models.CharField(max_length=1400)
+    created_on = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey('ChatRoom', on_delete=models.CASCADE)
