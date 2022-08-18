a9e338e06ade4e1b3a51f7be420bee2c7af1767b
KristianColville1 <kristiancolville1@gmail.com>
2022-08-18 14:37:58 +0000

    Create url connection to websocket and grab form input

--- static/js/chats.js
+++ static/js/chats.js
@@ -1,10 +1,18 @@
-$(document).ready(function () {
-    let url = `ws://${window.location.host}/ws/socket-server/`;
+let url = `wss://${window.location.host}/ws/socket-server/`;
 
-    const chatSocket = new WebSocket(url)
+const chatSocket = new WebSocket(url)
 
-    chatSocket.onmessage = (e) => {
-        let data = JSON.parse(e.data)
-        console.log('Data:', data)
-    }
+chatSocket.onmessage = (e) => {
+    let data = JSON.parse(e.data)
+    console.log('Data:', data)
+}
+
+let form = document.getElementById('chat-form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let message = e.target.message.value
+    chatSocket.send(JSON.stringify({
+        'message': message
+    }))
+    form.reset()
 });
