9775e9c17d9e1f809d0afce97dc4e50253f1e59f
KristianColville1 <kristiancolville1@gmail.com>
2022-08-19 13:21:10 +0000

    Added paths for routing, urls and updated views

--- chats/urls.py
+++ chats/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ChatView.as_view(), name='chat'),
+    path('<str:room_name>/', views.ChatRoomView.as_view(), name='room')
 ]
--- chats/views.py
+++ chats/views.py
@@ -6,5 +6,18 @@ class ChatView(View):
     """
     ChatView
     """
+
     def get(self, request):
         return render(request, 'chats/index.html')
+
+
+class ChatRoomView(View):
+    """
+    ChatRoomView
+    """
+
+    def get(self, request, room_name):
+        context = {
+            'room_name': room_name,
+        }
+        return render(request, 'chats/room.html', context)
