7bf92d1e5dab3e5521fe922de367724c9e77cba2
KristianColville1 <kristiancolville1@gmail.com>
2022-08-19 13:23:44 +0000

    Added document ready for chats.js

--- static/js/chats.js
+++ static/js/chats.js
@@ -5,60 +5,53 @@
  * 
  * Link to original source https: //channels.readthedocs.io/en/latest/tutorial/part_2.html
  */
+$(document).ready(function () {
+    /**
+     * Creating chat room 
+     */
+    $('#room-name-input').focus();
+    $('#room-name-input').on('keyup', (e) => {
+        if (e.KeyCode === 13) {
+            $('#room-name-submit').click();
+        }
+    });
 
-/**
- * Creating chat room 
- */
-$('#room-name-input').focus();
-$('#room-name-input').on('keyup', (e) => {
-    if (e.KeyCode === 13) {
-        $('#room-name-submit').click();
-    }
-});
+    $('#room-name-submit').on('click', (e) => {
+        var roomName = $('#room-name-input').val();
+        window.location.pathname = '/chats/' + roomName + '/';
+    });
 
-$('#room-name-submit').on('click', (e) => {
-    var roomName = $('#room-name-input').val();
-    window.location.pathname = '/chats/' + roomName + '/';
-});
+    /**
+     * Using chat room
+     */
+    const roomName = JSON.parse(document.getElementById('room-name').textContent);
+    const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    const chatSocket = new WebSocket(
+        protocol + '//' + window.location.host + '/ws/chat/' + roomName + '/'
+    );
 
-/**
- * Using chat room
- */
-const roomName = JSON.parse($('room-name').text());
+    chatSocket.onmessage = (e) => {
+        const data = JSON.parse(e.data);
+        $('#chat-log').val() += (data.message + '\n');
+    };
 
-/**
- * I'm using wss here because it works over http and https
- * and ws won't work over https.
- */
-const chatSocket = new WebSocket(
-    'wss://' +
-    window.location.host +
-    '/ws/chat/' +
-    roomName +
-    '/'
-);
+    chatSocket.onclose = (e) => {
+        console.error('Chat socket closed unexpectedly');
+    };
 
-chatSocket.onmessage = (e) => {
-    const data = JSON.parse(e.data);
-    $('#chat-log').val() += (data.message + '\n');
-};
+    $('#chat-message-input').focus();
+    $('#chat-message-input').on('keyup', (e) => {
+        if (e.keyCode === 13) { // enter, return
+            $('#chat-message-submit').click();
+        }
+    });
 
-chatSocket.onclose = (e) => {
-    console.error('Chat socket closed unexpectedly');
-};
-
-$('#chat-message-input').focus();
-$('#chat-message-input').on('keyup', (e) => {
-    if (e.keyCode === 13) { // enter, return
-        $('#chat-message-submit').click();
-    }
-});
-
-$('#chat-message-submit').on('click', (e) => {
-    const messageInputDom = $('#chat-message-input');
-    const message = messageInputDom.value;
-    chatSocket.send(JSON.stringify({
-        'message': message
-    }));
-    messageInputDom.value = '';
+    $('#chat-message-submit').on('click', (e) => {
+        const messageInputDom = $('#chat-message-input');
+        const message = messageInputDom.value;
+        chatSocket.send(JSON.stringify({
+            'message': message
+        }));
+        messageInputDom.value = '';
+    });
 });
