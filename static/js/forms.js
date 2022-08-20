aae60b890fadd898340c26b52820cd9f32c53ba1
KristianColville1 <kristiancolville1@gmail.com>
2022-08-20 16:13:11 +0100

    Added missing commas to forms.js

--- static/js/forms.js
+++ static/js/forms.js
@@ -3,9 +3,9 @@
  function collects the post id and manipulates
  the appropriate container with the element to select */
 var commentForm = (postID) => {
-    let selector = `.comment-form-${postID}`
+    let selector = `.comment-form-${postID}`;
     $(selector).removeClass('d-none');
-}
+};
 
 $(document).ready(() => {
     /**
@@ -14,7 +14,7 @@ $(document).ready(() => {
     let commentContainer = $('#div_id_comment');
     let comment = $('#id_comment');
     $(comment).addClass('dark-boxes rounded-5 text-center');
-    $(commentContainer).children().first().addClass('d-none')
+    $(commentContainer).children().first().addClass('d-none');
 
     /**
      * Adds create post modal form to template
@@ -25,7 +25,7 @@ $(document).ready(() => {
         <div class="modal-dialog">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title w-100 text-center" id="create-post">Create a post</h5>
+                <h1 class="modal-title w-100 text-center" id="create-post">Create a post</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -36,10 +36,10 @@ $(document).ready(() => {
                 <label for="post_body" class="w-100 text-center modal-label">Content</label>
                 <textarea name="post_body" rows="6" maxlength="500" required="" id="post_body" class="w-100 rounded"></textarea>
 
-                <button type="Submit" class="btn post-btn w-100 my-2 text-uppercase">Post</button>
+                <button type="Submit" class="btn general-btn w-100 my-2 text-uppercase" aria-label="Click here to submit post">Post</button>
                 </form>
             </div>
             </div>
         </div>
-    </div>`)
-})
+    </div>`);
+});
