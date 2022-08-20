ff25d6d977f40bd135bf1c737ec8549a36fc9afb
KristianColville1 <kristiancolville1@gmail.com>
2022-08-20 17:39:09 +0100

    Encrypt sensitive information and test heroku output

--- django_messages/models.py
+++ django_messages/models.py
@@ -5,6 +5,7 @@ from django.db.models import signals
 from django.utils import timezone
 from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+from django_cryptography.fields import encrypt
 
 AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
@@ -50,13 +51,28 @@ class Message(models.Model):
     """
     A private message from user to user
     """
-    subject = models.CharField(_("Subject"), max_length=140)
-    body = models.TextField(_("Body"))
-    sender = models.ForeignKey(AUTH_USER_MODEL, related_name='sent_messages', verbose_name=_("Sender"), on_delete=models.PROTECT)
-    recipient = models.ForeignKey(AUTH_USER_MODEL, related_name='received_messages', null=True, blank=True, verbose_name=_("Recipient"), on_delete=models.SET_NULL)
-    parent_msg = models.ForeignKey('self', related_name='next_messages', null=True, blank=True, verbose_name=_("Parent message"), on_delete=models.SET_NULL)
-    sent_at = models.DateTimeField(_("sent at"), null=True, blank=True)
-    read_at = models.DateTimeField(_("read at"), null=True, blank=True)
+    subject = encrypt(models.CharField(_("Subject"), max_length=100))
+    body = encrypt(models.TextField(_("Body"), max_length=1400))
+    sender = models.ForeignKey(AUTH_USER_MODEL,
+                               related_name='sent_messages',
+                               verbose_name=_("Sender"),
+                               on_delete=models.PROTECT)
+    recipient = models.ForeignKey(AUTH_USER_MODEL,
+                                  related_name='received_messages',
+                                  null=True,
+                                  blank=True,
+                                  verbose_name=_("Recipient"),
+                                  on_delete=models.SET_NULL)
+    parent_msg = models.ForeignKey('self',
+                          related_name='next_messages',
+                          null=True,
+                          blank=True,
+                          verbose_name=_("Parent message"),
+                          on_delete=models.SET_NULL)
+    sent_at = models.DateTimeField(_("sent at"), null=True,
+                                           blank=True)
+    read_at = models.DateTimeField(_("read at"), null=True,
+                                           blank=True)
     replied_at = models.DateTimeField(_("replied at"), null=True, blank=True)
     sender_deleted_at = models.DateTimeField(_("Sender deleted at"), null=True, blank=True)
     recipient_deleted_at = models.DateTimeField(_("Recipient deleted at"), null=True, blank=True)
@@ -97,9 +113,13 @@ def inbox_count_for(user):
     returns the number of unread messages for the given user but does not
     mark them seen
     """
-    return Message.objects.filter(recipient=user, read_at__isnull=True, recipient_deleted_at__isnull=True).count()
+    return Message.objects.filter(recipient=user,
+                                  read_at__isnull=True,
+                                  recipient_deleted_at__isnull=True).count()
+
 
 # fallback for email notification if django-notification could not be found
-if "pinax.notifications" not in settings.INSTALLED_APPS and getattr(settings, 'DJANGO_MESSAGES_NOTIFY', True):
+if "pinax.notifications" not in settings.INSTALLED_APPS and getattr(
+        settings, 'DJANGO_MESSAGES_NOTIFY', True):
     from django_messages.utils import new_message_email
     signals.post_save.connect(new_message_email, sender=Message)
