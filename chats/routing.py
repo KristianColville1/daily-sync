31a89920a81b0725c217ccad66797841ea3dcf25
KristianColville1 <kristiancolville1@gmail.com>
2022-08-20 10:20:00 +0100

    Fixed typo in rwebsocket patterns

--- chats/routing.py
+++ chats/routing.py
@@ -2,8 +2,8 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>)\w+/$',
+    re_path(r'ws/chat/(?P<room_name>\w+)/$',
             consumers.ChatsConsumer.as_asgi()),
-    re_path(r'wss/chat/(?P<room_name>)\w+/$',
+    re_path(r'wss/chat/(?P<room_name>\w+)/$',
             consumers.ChatsConsumer.as_asgi())
 ]
