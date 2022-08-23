0430256bda12bf774d2ccdb49827857b93cea264
KristianColville1 <kristiancolville1@gmail.com>
2022-08-23 15:03:37 +0100

    Tested responsivity and adjusted accordingly

--- static/js/forms.js
+++ static/js/forms.js
@@ -44,7 +44,7 @@ $('.share-post-btn').hover(function () {
     <form method="post" action="/posts/share_post/${post_id}">
         <input type="hidden" name="csrfmiddlewaretoken" value="${csrf_token}">
         <textarea name="post_body" rows="6" maxlength="500" required="" id="post_body" class="w-100 rounded" placeholder="Write here..."></textarea>
-        <h3 class="blue-like w-100 text-center">sharing:</h3>
+        <h3 class="w-100 text-center">sharing:</h3>
         <div class="w-100 dark-boxes border shadow-sm rounded post-to-share mt-3"></div>
         <button type="Submit" class="btn general-btn w-100 my-2 text-uppercase" aria-label="Click here to submit post">Share post</button>
     </form>
@@ -52,6 +52,9 @@ $('.share-post-btn').hover(function () {
     `;
     $('.modal-body').html(`${shareForm}`);
     let post_sharing = $('.post-to-share').html(to_share);
+    /* removes the button options at the end of the post the user is sharing*/
+    $('.post-to-share').find('.user-post-options').html('');
+    $('.post-to-share').find('.user-post-options').next().html('');
 });
 
 /* modal container */
