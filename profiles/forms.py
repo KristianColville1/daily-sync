33d4a8af843605c10fd11ca8eceed20cc865f526
KristianColville1 <kristiancolville1@gmail.com>
2022-08-26 19:30:40 +0100

    Created an profile edit form

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,22 +1,41 @@
 from django import forms
+from cloudinary.forms import CloudinaryFileField 
+
 from .models import Profile
 
-
-class ProfileForm(forms.ModelForm):
+class EditProfileForm(forms.ModelForm):
     """
     Creates a form object for the profile model
     """
 
     class Meta:
         model = Profile
-        fields = (
+        fields = [
             'first_name',
             'last_name',
             'email',
             'd_o_b',
+            'bio',
             'avatar',
             'background',
-        )
+        ]
         widgets = {
-            'First name'
+            'First name',
+                forms.CharField(),
+            'Last name',
+                forms.CharField(),
+            'Email',
+                forms.Textarea(attrs={
+                'rows': 1,
+            }),
+            'Date_of_Birth',
+                forms.DateField(),
+            'Bio',
+                forms.Textarea(attrs={
+                'rows': 3,
+            }),
+            'Avatar',
+                CloudinaryFileField(),
+            'Background',
+                CloudinaryFileField(),
         }
