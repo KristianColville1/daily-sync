0e0870555c94911e899793ece038c8590258ed52
KristianColville1 <kristiancolville1@gmail.com>
2022-08-27 20:58:31 +0100

    Added add friend url to profile urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,14 +3,15 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path('', login_required(views.ProfileOwnerView.as_view()), name='profile'),
-    path('view_profile/<slug:slug>/',
+     path('', login_required(views.ProfileOwnerView.as_view()), name='profile'),
+     path('view_profile/<slug:slug>/',
          login_required(views.OtherProfileView.as_view()),
          name='view_profile'),
-    path('follow_profile/<int:profile_id>/',
+     path('follow_profile/<int:profile_id>/',
          login_required(views.follow_profile),
          name='follow'),
-    path('user_profiles/',
+     path('add_friend/', views.add_friend, name="add_friend"),
+     path('user_profiles/',
          login_required(views.AllProfilesView.as_view()),
          name='user_profiles'),
 ]
