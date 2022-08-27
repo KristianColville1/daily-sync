dd8b323ce65b46cfbb9752948f07d2e6b797429e
KristianColville1 <kristiancolville1@gmail.com>
2022-08-27 17:55:59 +0100

    Removed xframe options temporarily to create responsive imgs

--- daily_sync/settings.py
+++ daily_sync/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = is_boolean
 
-X_FRAME_OPTIONS = "SAMEORIGIN"
+
 
 ALLOWED_HOSTS = ["daily-sync123.herokuapp.com", "localhost", "127.0.0.1"]
 
